50e13893f00e329a0e459c2fcf4e96d1e4e07a5e
Muhammad Bilal <bilalroze94@gmail.com>
2022-10-27 22:40:26 +0500

    v1 commit

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hi!");
+  let today = new Date();
+  let currentDay = today.getDay();
+
+  if (currentDay === 6 || currentDay === 0) {
+    res.write("<h1>Yay, it's the weekend!</h1>");
+  } else {
+    res.sendFile(__dirname + "/index.html");
+  }
 });
 
 app.listen(3000, function () {
