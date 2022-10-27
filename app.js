228ec976ae21584dad22339b7ff1b60482e94a58
Muhammad Bilal <bilalroze94@gmail.com>
2022-10-28 00:26:38 +0500

    CSS commit

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let items = ["Eat 🍖", "Sleep 😴", "Code 🖥️"];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
   let today = new Date();
